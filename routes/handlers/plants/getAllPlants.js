7762ef743562a6005d79f2adab831f65087aa550
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-12-19 08:49:22 +0700

    Done

--- routes/handlers/plants/getAllPlants.js
+++ routes/handlers/plants/getAllPlants.js
@@ -2,7 +2,9 @@ const { Plant } = require("../../../models");
 
 // Get all users data
 module.exports = async (req, res) => {
-  const plants = await Plant.findAll({ attributes: ["id", "name", "soil"] });
+  const plants = await Plant.findAll({
+    attributes: ["id", "name", "soil", "latin_name"],
+  });
 
   return res.json({
     meta: {
